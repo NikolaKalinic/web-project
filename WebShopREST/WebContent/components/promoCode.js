6e94f01d73402331bb0cfe45bf54d9e62e92602a
Stefan Lepsanovic <lepsanovic.ra161.2019@uns.ac.rs>
2022-06-30 17:50:30 +0200

    feature promo codes added

--- WebShopREST/WebContent/components/promoCode.js
+++ WebShopREST/WebContent/components/promoCode.js
@@ -1,7 +1,14 @@
 Vue.component("promo-code",{
 	data:function(){
 		return{
-			
+			promoCodes: null,
+			validPromoCodes: null,
+			name: null,
+			dateStart: null,
+			dateEnd: null,
+			numberOfUse: null,
+			percentage: null,
+			mode: 'BROWSE'			
 		}
 	},
 	template:
@@ -24,6 +31,123 @@ Vue.component("promo-code",{
 			    </div>
 			  </div>
 			</nav>
+			<div class="container">
+				<div class="row"  >
+					<div class="justify-content-center">
+				  		<div class="row">
+				   			<div class="col-md-6 col-sm-6 col-xs-12 ">
+								<div class="d-flex justify-content-center">
+								  <button v-bind:hidden="this.mode=='NEW'" class="btn btn-primary" v-on:click="newPromoCodeButtonClick()">New promo code</button>
+								</div>
+				    		<!-- Form code begins -->
+								<form>
+								<div class="form-group"> 
+									<label v-bind:hidden="this.mode=='BROWSE'" class="control-label text-center" for="date">Name</label>
+									<input v-bind:hidden="this.mode=='BROWSE'" class="form-control" v-model="name" name="name"
+									v-model="name"
+									id="name"   type="text"/>
+								</div>
+								<div class="form-group"> 
+									<label v-bind:hidden="this.mode=='BROWSE'" class="control-label" for="date">Date start</label>
+									<input v-bind:hidden="this.mode=='BROWSE'" class="form-control"  name="dateStart"
+									v-model="dateStart"
+									id="dateStart"   type="date"/>
+								</div>
+								<div class="form-group"> 
+									<label v-bind:hidden="this.mode=='BROWSE'" class="control-label" for="date">Date end</label>
+									<input v-bind:hidden="this.mode=='BROWSE'" class="form-control" name="dateEnd"
+									v-model="dateEnd"
+									id="dateEnd"   type="date"/>
+								</div>
+								<div class="form-group"> 
+									<label v-bind:hidden="this.mode=='BROWSE'" class="control-label" for="date">Number of use</label>
+									<input v-bind:hidden="this.mode=='BROWSE'" class="form-control"  name="numberOfUse"
+									v-model="numberOfUse"
+									id="numberOfUse"  type="text"/>
+								</div>
+								<div class="form-group"> 
+									<label v-bind:hidden="this.mode=='BROWSE'" class="control-label" for="date">Sale</label>
+									<input v-bind:hidden="this.mode=='BROWSE'" class="form-control" name="percentage"
+									v-model="percentage"
+									id="percentage"  type="text"/>
+								</div>
+								
+								<div class="form-group"> <!-- Submit button -->
+									<button v-bind:hidden="this.mode=='BROWSE'" class="btn btn-primary " v-on:click="newPromoCode()" name="submit" type="submit">Submit</button>
+								</div>
+								</form>
+								<!-- Form code ends --> 
+							</div>
+				    	</div>
+				  	</div>    
+				</div>				
+				<div class="row">
+		    		<div class="col">
+			    		<table class="table table-striped table-dark">
+			    			<thead>
+				    		<tr>
+				    			<th>Name</th>
+				    			<th>Period</th>
+				    			<th>Number of use</th>
+				    			<th>Sale</th>				    				
+				    		</tr>
+				    		</thead>
+				    		<tbody>
+				    		<tr v-for="pc in promoCodes" v-if="pc.dateEnd > new Date()">
+				    			<td>{{pc.name}}</td>
+				    			<td>{{pc.dateStart | dateFormat('DD.MM.YYYY.')}} - {{pc.dateEnd | dateFormat('DD.MM.YYYY.')}}</td>
+				    			<td>{{pc.numberOfUse}}</td>
+				    			<td>{{pc.percentage}}%</td>				    				
+				    		</tr>
+				    		</tbody>
+				    	</table>    
+				    </div>
+			    </div>
+			</div>
+			
+		</div>
+		
 	</div>
-	`
+		
+	`,
+	mounted(){				
+		axios
+		.get('rest/promo-code')
+		.then(response => (this.promoCodes = response.data));		
+	},
+	methods:{
+		showDate: function(){
+			alert(this.dateStart);
+		},
+		newPromoCode: function() {
+            axios.post(
+                "rest/promo-code",
+                {
+                    name: this.name,
+                    dateStart: this.dateStart,
+                    dateEnd: this.dateEnd,
+                    numberOfUse: this.numberOfUse,
+                    percentage: this.percentage,
+                    
+                }
+            )
+            .then( response =>{
+               // window.location.href = 'sportsObjects.html';
+               alert("New promo code added!");
+            });
+            this.mode = "BROWSE";
+        },
+        newPromoCodeButtonClick: function(){
+			this.mode = 'NEW';
+		}
+		 
+		
+	},
+	filters: {
+    	dateFormat: function (value, format) {
+    		var parsed = moment(value);
+    		return parsed.format(format);
+    	}
+   	},
+   	
 });
--- WebShopREST/src/beans/PromoCode.java
+++ WebShopREST/src/beans/PromoCode.java
@@ -1,63 +1,75 @@
 package beans;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class PromoCode {
 	private int id;
 	private String name;
-	private String period;
+	private Date dateStart;
+	private Date dateEnd;
 	private int numberOfUse;
 	private int percentage;
+	
+	
 	
 	public PromoCode() {
 		
 	}
-	
-	public PromoCode(int id, String name, String period, int numberOfUse, int percentage) {
+	public PromoCode(int id, String name, Date dateStart, Date dateEnd, int numberOfUse, int percentage) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.period = period;
+		this.dateStart = dateStart;
+		this.dateEnd = dateEnd;
 		this.numberOfUse = numberOfUse;
 		this.percentage = percentage;
 	}
-
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public String getPeriod() {
-		return period;
+	public Date getDateStart() {
+		return dateStart;
 	}
-
-	public void setPeriod(String period) {
-		this.period = period;
+	public void setDateStart(Date dateStart) {
+		this.dateStart = dateStart;
 	}
-
+	public Date getDateEnd() {
+		return dateEnd;
+	}
+	public void setDateEnd(Date dateEnd) {
+		this.dateEnd = dateEnd;
+	}
 	public int getNumberOfUse() {
 		return numberOfUse;
 	}
-
 	public void setNumberOfUse(int numberOfUse) {
 		this.numberOfUse = numberOfUse;
 	}
-
 	public int getPercentage() {
 		return percentage;
 	}
-
 	public void setPercentage(int percentage) {
 		this.percentage = percentage;
+	}
+	
+	public String displayDateStart() {		
+		SimpleDateFormat sdf = new SimpleDateFormat("DD.MM.YYYY.");
+		return sdf.format(dateStart);
+	}
+	
+public String displayDateEnd() {		
+		SimpleDateFormat sdf = new SimpleDateFormat("DD.MM.YYYY.");
+		return sdf.format(dateEnd);
 	}
 	
 	
